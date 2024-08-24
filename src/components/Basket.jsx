c6794424a6451bd912d7932f9cf6a7afb3eec49d
Sevak <sevakrej@gmail.com>
2024-08-24 09:21:18 +0300

    Add BasketItem component to Basket

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,3 +1,5 @@
+import { BasketItem } from "./BasketItem"
+
 export const Basket = ({cart, onCountPlus,onCountMinus,onDelete}) => {
     return <div className="col-md-5">
         <h3>Basket: {cart.length}</h3>
@@ -16,25 +18,12 @@ export const Basket = ({cart, onCountPlus,onCountMinus,onDelete}) => {
                 </tr>
             </thead>
             <tbody>
-                {
-                    cart.map(elm => <tr key={elm.id}>
-                        <td>{elm.name}</td>
-                        <td>{elm.price} $</td>
-                        <td>{elm.count}</td>
-                        <td>{elm.count * elm.price} $</td>
-                        
-                            <td 
-                            style={{
-                                display: "flex",
-                                gap: "4px",
-                            }}
-                            >
-                                <button onClick={() => onCountMinus(elm)} className="btn btn-outline-info">-</button>
-                                <button onClick={() => onCountPlus(elm)} className="btn btn-outline-info">+</button>
-                                <button onClick={() => onDelete(elm.id)} className="btn btn-outline-info">x</button>
-                            </td>
-                    </tr>)
-                }
+                <BasketItem 
+                    cart={cart} 
+                    key={cart.id}
+                    onCountMinus={onCountMinus} 
+                    onCountPlus={onCountPlus} 
+                    onDelete={onDelete} />
             </tbody>
         </table>
         </div>
--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,5 +1,23 @@
-export const BasketItem = () => {
+export const BasketItem = ({cart ,onCountMinus, onCountPlus, onDelete}) => {
     return <>
-        <p>Basket Item</p>
+        {
+            cart.map(elm => <tr key={elm.id}>
+                <td>{elm.name}</td>
+                <td>{elm.price} $</td>
+                <td>{elm.count}</td>
+                <td>{elm.count * elm.price} $</td>
+                
+                    <td 
+                    style={{
+                        display: "flex",
+                        gap: "4px",
+                    }}
+                    >
+                        <button onClick={() => onCountMinus(elm)} className="btn btn-outline-info">-</button>
+                        <button onClick={() => onCountPlus(elm)} className="btn btn-outline-info">+</button>
+                        <button onClick={() => onDelete(elm.id)} className="btn btn-outline-info">x</button>
+                    </td>
+            </tr>)
+        }
     </>
 }
