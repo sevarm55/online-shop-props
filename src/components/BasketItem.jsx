6c59a953d4b7a3f57ee08d1b4ac55f8b17970714
Sevak <sevakrej@gmail.com>
2024-08-25 13:39:12 +0300

    Fix Basket rendering to display items correctly

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,5 +1,18 @@
-export const BasketItem = () => {
-    return <>
-        <p>Basket Item</p>
-    </>
+export const BasketItem = ({id,name, price, count, onCountMinus, onCountPlus, onDelete}) => {
+    return <tr>
+    <td>{name}</td>
+    <td>{price} $</td>
+    <td>{count}</td>
+    <td>{count * price} $</td>
+        <td 
+        style={{
+            display: "flex",
+            gap: "4px",
+        }}
+        >
+            <button onClick={() => onCountMinus(id)} className="btn btn-outline-info">-</button>
+            <button onClick={() => onCountPlus(id)} className="btn btn-outline-info">+</button>
+            <button onClick={() => onDelete(id)} className="btn btn-outline-info">x</button>
+        </td>
+</tr>
 }
