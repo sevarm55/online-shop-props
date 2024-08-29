3a8580d19b05ce74dc24dd15d0076f02fecd2a1e
Sevak <sevakrej@gmail.com>
2024-08-29 22:45:52 +0300

    add useEffect in component Basket

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const BasketItem = ({cart ,onCountMinus, onCountPlus, onDelete}) => {
     return <>
         {
@@ -21,23 +20,4 @@ export const BasketItem = ({cart ,onCountMinus, onCountPlus, onDelete}) => {
             </tr>)
         }
     </>
-=======
-export const BasketItem = ({id,name, price, count, onCountMinus, onCountPlus, onDelete}) => {
-    return <tr>
-    <td>{name}</td>
-    <td>{price} $</td>
-    <td>{count}</td>
-    <td>{count * price} $</td>
-        <td 
-        style={{
-            display: "flex",
-            gap: "4px",
-        }}
-        >
-            <button onClick={() => onCountMinus(id)} className="btn btn-outline-info">-</button>
-            <button onClick={() => onCountPlus(id)} className="btn btn-outline-info">+</button>
-            <button onClick={() => onDelete(id)} className="btn btn-outline-info">x</button>
-        </td>
-</tr>
->>>>>>> 6c59a953d4b7a3f57ee08d1b4ac55f8b17970714
 }
